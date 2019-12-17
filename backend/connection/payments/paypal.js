f9394701ee5c28317ef74a3b76e0dc4d2b0bae24
kaushil shah <kaushil99@gmail.com>
2019-12-17 15:13:09 +0530

    hide

--- backend/connection/payments/paypal.js
+++
@@ -1,8 +0,0 @@
-const paypal_credentials = {
-    'mode': 'sandbox',
-    'client_id': "AQ1JZRHCWMU95AIpRKEhua1SPF1clfXR4Xm1vuN5-4_bXNDYXZlrqHq6nrNfhyBvKWQh242lEVPUipr5",
-    'client_secret': "ELLcxkNl2I2B-fAqF9X912yuLoJmWI0Gl2ILAmy1L6SsQclttfnJCL7WdYJT0qez-_xOxMdV1FIgP--0"
-};
-
-module.exports = paypal_credentials;
-
